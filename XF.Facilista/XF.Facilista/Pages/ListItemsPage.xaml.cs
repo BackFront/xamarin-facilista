f9d7618057e2ee44e0005ba627c3ba3d7c9de123
Douglas Alves <douglas.alves_desenv@hotmail.com>
2019-07-18 20:40:05 -0300

    first commig

---
+++ XF.Facilista/XF.Facilista/Pages/ListItemsPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Facilista.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListItemsPage : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public ListItemsPage()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "Lista de compras mensal",
+                "Churrasco aniversário",
+                "Open house"
+            };
+			
+			Lista.ItemsSource = Items;
+        }
+
+        //async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        //{
+        //    if (e.Item == null)
+        //        return;
+
+        //    await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+        //    //Deselect Item
+        //    ((ListView)sender).SelectedItem = null;
+        //}
+        async void ListaClicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new Pages.MercadoriaListPage());
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        async void ListaAddClicked(object sender, EventArgs e) => await DisplayAlert("Nova lista", "Criar nova lista", "OK");
+    }
+}
---
+++ XF.Facilista/XF.Facilista/Pages/MercadoriaListPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.Facilista.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MercadoriaListPage : ContentPage
+	{
+        public ObservableCollection<string> Mercadorias { get; set; }
+
+        public MercadoriaListPage()
+		{
+			InitializeComponent();
+
+            Mercadorias = new ObservableCollection<string>
+            {
+                "Carne",
+                "Cerveja",
+                "Refrigerante"
+            };
+
+            Itens.ItemsSource = Mercadorias;
+        }
+
+        //async void MercadoriaClicked(object sender, ItemTappedEventArgs e)
+        //{
+        //    if (e.Item == null) return;
+
+        //    await DisplayAlert("Clicou na mercadoria", "Mostar os detalhes da mercadoria clicada", "OK");
+
+        //    ((ListView)sender).SelectedItem = null;
+        //}
+
+        async void MercadoriaClicked(object sender, EventArgs e)
+        {
+
+            await Navigation.PushAsync(new Pages.MercadoriaUpdatePage());
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
